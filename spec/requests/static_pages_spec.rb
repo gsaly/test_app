f1548f3fd3e34b82a175a9c99f45ff31e721e3e7
gsaly <gueyesaly@yahoo.fr>
2012-06-02 03:57:14 +0200

    update tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require "rspec"
 describe "Static pages" do
 
   describe "Home page" do
-
+=begin
     it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title', :text => "Ruby on Rails Test App")
@@ -14,6 +14,26 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should_not have_selector('title', :text => '| Home')
     end
+=end
+    it "should have the content 'BIENVENUE'" do
+      visit '/static_pages/home'
+      page.should have_content('BIENVENUE')
+    end
+  end
+
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => '| Help')
+    end
+
   end
 
 end
